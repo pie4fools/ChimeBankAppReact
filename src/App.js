5fc3c38c60c8ea1b7a0503c9fda9b7a5de1a2dda
pie4fools <64813630+pie4fools@users.noreply.github.com>
2022-12-14 10:20:18 -0600

    Set up mobile menu with page links and sign up btn

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Test</h1>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' />
+      </Routes>
+    </Router>
   );
 }
 
