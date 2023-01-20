6796639926b582ee67efdf14a0174650efc56ab1
pie4fools <64813630+pie4fools@users.noreply.github.com>
2023-01-20 07:50:04 -0600

    Updated path, website was in subfolder

---
+++ build/precache-manifest.84ca51a299d75115621bcd2c3f20982f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a2be4b98a4b234e2e08",
+    "url": "/react-website-v2/static/css/main.ea98b971.chunk.css"
+  },
+  {
+    "revision": "1a2be4b98a4b234e2e08",
+    "url": "/react-website-v2/static/js/main.1a2be4b9.chunk.js"
+  },
+  {
+    "revision": "7da7581c638d876bc209",
+    "url": "/react-website-v2/static/js/1.7da7581c.chunk.js"
+  },
+  {
+    "revision": "cc5f75de03e85d14813f",
+    "url": "/react-website-v2/static/js/runtime~main.cc5f75de.js"
+  },
+  {
+    "revision": "c8b29f3a7c6e34cf027f06afaf65e783",
+    "url": "/react-website-v2/index.html"
+  }
+];
