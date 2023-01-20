d1f971efe85b110320229f241f55d9981e84e9d6
pie4fools <64813630+pie4fools@users.noreply.github.com>
2023-01-20 08:49:12 -0600

    Trying to work on Vercel

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Home';
 import Services from './pages/Services/Services';
 import Products from './pages/Products/Products';
